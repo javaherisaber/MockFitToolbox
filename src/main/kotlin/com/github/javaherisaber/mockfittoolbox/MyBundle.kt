0b6550141a1a6c6e29925d8f1a05215665b27e0d
GitHub Action <action@github.com>
2020-11-06 18:14:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/javaherisaber/mockfittoolbox/MyBundle.kt
+++ src/main/kotlin/com/github/javaherisaber/mockfittoolbox/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.javaherisaber.mockfittoolbox
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/javaherisaber/mockfittoolbox/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.javaherisaber.mockfittoolbox.services
+
+import com.github.javaherisaber.mockfittoolbox.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
